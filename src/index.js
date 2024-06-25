470ba85b088a937ce6b41d83738deaf9aaecf651
JinJiangHuang <1062951657@qq.com>
2024-06-25 14:41:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.llsv.xyz": "https://registry-1.docker.io",
+  "quay.llsv.xyz": "https://quay.io",
+  "gcr.llsv.xyz": "https://gcr.io",
+  "k8s-gcr.llsv.xyz": "https://k8s.gcr.io",
+  "k8s.llsv.xyz": "https://registry.k8s.io",
+  "ghcr.llsv.xyz": "https://ghcr.io",
+  "cloudsmith.llsv.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
